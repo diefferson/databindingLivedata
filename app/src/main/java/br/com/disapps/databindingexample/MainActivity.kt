1c09ce3245f4388b9516f0872c692060cae4cd71
Diefferson Santos <diefferson.sts@gmail.com>
2018-03-18 11:15:48 -0300

    Converto to binding of livedata

--- app/src/main/java/br/com/disapps/databindingexample/MainActivity.kt
+++ app/src/main/java/br/com/disapps/databindingexample/MainActivity.kt
@@ -1,30 +1,23 @@
 package br.com.disapps.databindingexample
 
 import android.arch.lifecycle.ViewModelProviders
-import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import br.com.disapps.databindingexample.databinding.ActivityMainBinding
-import kotlinx.android.synthetic.main.activity_main.*
+import android.databinding.DataBindingUtil.setContentView
 
 class MainActivity : AppCompatActivity() {
 
-    private val binding: ActivityMainBinding by lazy {
-        DataBindingUtil.bind<ActivityMainBinding>(viewRoot)
-    }
-
-    private val viewModel: MainViewModel by lazy {
-        ViewModelProviders.of(this).get(MainViewModel::class.java)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        binding.data = viewModel.data
+        val binding: ActivityMainBinding? = setContentView(this, R.layout.activity_main)
+        val viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
 
-        button1.setOnClickListener { viewModel.addvalue1() }
-        button2.setOnClickListener { viewModel.addValue2() }
-        button3.setOnClickListener { viewModel.reset() }
+        binding?.let {
+            it.viewModel = viewModel
+            it.setLifecycleOwner(this)
+        }
+
     }
 }
--- app/src/main/java/br/com/disapps/databindingexample/MainViewModel.kt
+++ app/src/main/java/br/com/disapps/databindingexample/MainViewModel.kt
@@ -1,24 +1,27 @@
 package br.com.disapps.databindingexample
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import android.view.View
 
 /**
  * Created by diefferson on 31/10/17.
  **/
 class MainViewModel: ViewModel(){
 
-    var data = Data()
+    val value1 = MutableLiveData<Int>().apply { value = 0 }
+    val value2 = MutableLiveData<Int>().apply { value = 0 }
 
-    fun addvalue1(){
-        data.value1++
+    fun addValue1(view : View){
+       value1.value = value1.value?.plus(1)
     }
 
-    fun addValue2(){
-       data.value2++
+    fun addValue2(view : View){
+        value2.value = value2.value?.plus(1)
     }
 
-    fun reset(){
-        data.value1 = 0
-        data.value2 = 0
+    fun reset(view : View){
+        value1.value = 0
+        value2.value = 0
     }
 }
